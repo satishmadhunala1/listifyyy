1895b576e2d54214c711f0a43e151693ba3e8dd4
satishmadhunala1 <satishmadhunala2001@gmail.com>
2026-02-05 17:00:35 +0530

    changes in api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,10 +4,12 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth";
 
+
+
 // Create axios instance
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || API_URL,
-  headers: {
+   baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
+    headers: {
     "Content-Type": "application/json",
   },
   timeout: 30000, // Increased timeout to 30 seconds
